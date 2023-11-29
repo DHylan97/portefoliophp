1020a50d868edcce9f18510b9eaac9e55504e166
Dylan Haas <86422525+DHylan97@users.noreply.github.com>
2023-11-29 14:29:17 +0100

    Update aboutme.php

--- views/aboutme.php
+++ views/aboutme.php
@@ -3,7 +3,6 @@
     <div class="text-container">
       <h2 class="blue">A propos de moi</h2>
       <p>Formé de base comme secrétaire, je me suis reconverti dans le webdesign.</p>
-      <p>je sais pas encore quoi écrire d'autre</p>
     </div>
   </div>
 </section>
